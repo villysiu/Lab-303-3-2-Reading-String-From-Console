2400f61058e4aaa46e178f3ca3445c77e88ec3c6
Villy Siu <villysiu@gmail.com>
2024-12-05 14:39:40 -0800

    added

--- src/ReadStringFromConsole2.java
+++ src/ReadStringFromConsole2.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ReadStringFromConsole2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        String s = input.nextLine();
+        char ch = s.charAt(0);
+        System.out.println("The character entered is " + ch);
+        input.close();
+    }
 }
